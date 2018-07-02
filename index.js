d9dd25414323d475df390b029caee33c1e169834
jaanhio <jianhaot.89@gmail.com>
2018-07-02 14:20:46 +0800

    added photo taking feature

---
+++ index.js
@@ -0,0 +1,50 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // ensure script is loading
+  console.log('dom loaded');
+
+  const removeDebugContainerDisplay = () => {
+    console.log('removing display');
+    let debugContainer = document.getElementById('arjsDebugUIContainer');
+    debugContainer.style.display = 'none';
+  };
+
+  function vibrateDevice() {
+    window.navigator.vibrate(200);
+    console.log('device vibrated');
+  };
+
+  const getScreenShot = () => {
+    const vidStream = document.getElementsByTagName('video')[0];
+    console.log('vidStream', vidStream);
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    const captureButton = document.getElementById('screenshot');
+
+    const constraints = {
+      video: true,
+    };
+
+    captureButton.addEventListener('click', () => {
+      context.drawImage(vidStream, 0, 0, canvas.width, canvas.height);
+      let dl = document.createElement('a');
+      dl.href = canvas.toDataURL();
+      dl.download = 'Image';
+      dl.click();
+      alert('download clicked');
+    });
+
+    navigator.mediaDevices.getUserMedia(constraints)
+      .then((stream) => {
+        vidStream.srcObject = stream;
+      });
+  }
+
+  const runModScripts = () => {
+    removeDebugContainerDisplay();
+    vibrateDevice();
+    getScreenShot();
+  }
+
+  setTimeout(runModScripts, 8000);
+
+});
